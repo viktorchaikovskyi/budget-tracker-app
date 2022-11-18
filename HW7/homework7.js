178640d9f4f8dcdfffb7c0eec14f2e419ea082eb
Viktor <68876230+viktorchaikovskyi@users.noreply.github.com>
2022-11-18 20:36:17 +0200

    HW7 (6/7)

--- HW7/homework7.js
+++ HW7/homework7.js
@@ -1,6 +1,6 @@
 // Task 1
 
-/* let newWin = window.open('/', 'task 1', 'width=300,height=300');
+let newWin = window.open('/', 'task 1', 'width=300,height=300');
 setTimeout (function() {
     newWin.resizeTo(width=500, height=500);
     setTimeout (function() {
@@ -9,19 +9,19 @@ setTimeout (function() {
             newWin.close()
         }, 2000)
     }, 2000)
-}, 2000); */
+}, 2000);
 
 
 // Task 2
 
-/* function changeCSS() {
+function changeCSS() {
     document.getElementById("text").style = "color: orange; font-size: 20px; font-family: Comic Sans MS"
-} */
+}
 
 
 // Task 3
 
-/* let changeBackground1 = () => document.querySelector("#task3").style = "background-color: blue";
+let changeBackground1 = () => document.querySelector("#task3").style = "background-color: blue";
 let changeBackground2 = () => document.querySelector("#task3").style = "background-color: pink";
 let changeBackground3 = () => document.querySelector("#task3").style = "background-color: brown";
 let changeBackground4 = () => document.querySelector("#task3").style = "background-color: yellow";
@@ -32,22 +32,32 @@ btn2.addEventListener("dblclick", changeBackground2);
 btn3.addEventListener("mousedown", changeBackground3)
 btn3.addEventListener("mouseup", changeBackground5);
 link.addEventListener ("mouseover", changeBackground4);
-link.addEventListener ("mouseout", changeBackground5); */
+link.addEventListener ("mouseout", changeBackground5);
+
+
+// Task 4
+
+document.getElementById("submit").onclick = function () {
+    let sel = document.querySelector("#names").selectedOptions;
+    for (let i = 0; i < sel.length; i++) {
+        sel[i].remove()
+    }
+};
 
 
 // Task 5
 
-/* document.querySelector("#livebtn").onclick = function () {
-    message.insertAdjacentHTML("beforeend", "<p>I was pressed!</p>") 
+document.querySelector("#livebtn").onclick = function () {
+    message.insertAdjacentHTML("beforeend", "<p>I was pressed!</p>")
 };
 document.querySelector("#livebtn").onmouseover = function() {
     message.insertAdjacentHTML("beforeend", "<p>Mouse on me!</p>")
 };
 document.querySelector("#livebtn").onmouseout = function() {
     message.insertAdjacentHTML("beforeend", "<p>Mouse is not on me!</p>")
-}; */
+};
 
 
 // Task 6
 
-/* window.onresize = () => document.getElementById("window-size").innerHTML = `Width: ${window.innerWidth}, Height: ${window.innerHeight}` */
+window.onresize = () => document.getElementById("window-size").innerHTML = `Width: ${window.innerWidth}, Height: ${window.innerHeight}`
