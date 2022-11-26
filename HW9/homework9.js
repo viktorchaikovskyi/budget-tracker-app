bd38dc572eecc0b4f936579d9607d2cabde0fc65
Viktor <68876230+viktorchaikovskyi@users.noreply.github.com>
2022-11-26 13:48:37 +0200

    HW9

--- HW9/homework9.js
+++ HW9/homework9.js
@@ -5,6 +5,22 @@ $('a[href^="https://"]').attr('target', '_blank');
 let h2 = $('h2.head').css('background-color', 'green');
 h2 = $('.inner').css('font-size', '35px');
 
-$('h3').next('div').css("color", "red");
+
 // let findDiv = $('h3').next('div').insertBefore('h3')
 // $(findDiv).insertBefore('h3');
+let divs = $('h3').next('div').css("color", "red"); // css прописав просто для виділення
+function moveDiv() {
+    for (let i = 0; i < divs.length; i++) {
+    $('h3')[i].before(divs[i]);
+    }
+}
+moveDiv()
+
+let disableCheckbox = function() {
+$('input[type="checkbox"]').on('change', function() {
+    if ($('input:checked').length >= 3) {
+        $('[type="checkbox"]').attr('disabled', true);
+                }
+    });
+};
+disableCheckbox()
