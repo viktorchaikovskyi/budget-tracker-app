7d2a15bc7991df97427fdd850541ffe32d4b717e
Viktor <68876230+viktorchaikovskyi@users.noreply.github.com>
2023-01-04 17:03:42 +0200

    1st task done

--- HW11/server.js
+++ HW11/server.js
@@ -6,10 +6,10 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     
     response.write("<h2>System information</h2>");
-    response.write("Current user name: " + os.userInfo().username);
-    response.write("OS type: " + os.type());
-    response.write("System work time: " + (os.uptime()/60).toFixed(2) + "minutes");
-    response.write("Current work directory: " + path.dirname(__filename));
+    response.write("Current user name: " + os.userInfo().username + "<br>");
+    response.write("OS type: " + os.type() + "<br>");
+    response.write("System work time: " + (os.uptime()/60).toFixed(2) + "minutes <br>"); 
+    response.write("Current work directory: " + path.dirname(__filename) + "<br>");
     response.end("Server file name: " + path.basename(__filename));
   }).listen(5000);
 
